5f5d7205a2e2dc5b42f932d09e56deeb08048f06
墨冰 <mobing@mogujie.com>
2017-04-24 14:24:44 +0800

    initfirst

---
+++ app/src/main/java/com/mobing/test/ActivityB.java
@@ -0,0 +1,43 @@
+package com.mobing.test;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Explode;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ActivityB extends AppCompatActivity implements View.OnClickListener{
+
+    private ViewGroup mRootView;
+    private View mRedBox, mGreenBox, mBlueBox, mBlackBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+
+        mRootView = (ViewGroup) findViewById(R.id.layout_root_view);
+        mRootView.setOnClickListener(this);
+
+        mRedBox = findViewById(R.id.red_box);
+        mGreenBox = findViewById(R.id.green_box);
+        mBlueBox = findViewById(R.id.blue_box);
+        mBlackBox = findViewById(R.id.black_box);
+    }
+
+    @Override
+    public void onClick(View v) {
+//        TransitionManager.beginDelayedTransition(mRootView, new Fade());
+//        TransitionManager.beginDelayedTransition(mRootView, new Slide());
+        TransitionManager.beginDelayedTransition(mRootView, new Explode());
+        toggleVisibility(mRedBox, mGreenBox, mBlueBox, mBlackBox);
+    }
+
+    private static void toggleVisibility(View... views) {
+        for (View view : views) {
+            boolean isVisible = view.getVisibility() == View.VISIBLE;
+            view.setVisibility(isVisible ? View.INVISIBLE : View.VISIBLE);
+        }
+    }
+}
